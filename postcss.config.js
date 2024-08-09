6e18cf1c3e74e9e5a20aeadf244477b038636c94
JUNG HEE CHUL <da.yyy.pa@gmail.com>
2024-08-09 21:57:29 +0900

    1

--- postcss.config.js
+++ postcss.config.js
@@ -1,2 +1,13 @@
 // postcss.config.js
-export const plugins = ['tailwindcss', 'autoprefixer'];
+// const plugins = ['tailwindcss', 'autoprefixer'];
+
+module.exports = {
+  plugins: {
+    'postcss-preset-env': {
+      stage: 1,
+      features: {
+        'nesting-rules': true,
+      },
+    },
+  },
+};
